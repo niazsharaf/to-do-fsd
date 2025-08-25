10e4ea6675883848a00706dcfa21d43da8fffe0b
niaz <niazsik@gmail.com>
2025-08-25 17:39:48 +0300

    bug: fix github actiobs

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,5 +3,5 @@ import { BrowserRouter } from 'react-router-dom'
 import type { ReactNode } from 'react'
 
 export function AppProviders({ children }: { children: ReactNode }) {
-  return <BrowserRouter>{children}</BrowserRouter>
+  return <BrowserRouter basename="/to-do-fsd">{children}</BrowserRouter>
 }
