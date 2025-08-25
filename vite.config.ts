8061c9626bfd893c189ed9bb0568886500c78723
niaz <niazsik@gmail.com>
2025-08-25 16:39:36 +0300

    feat: add support vite github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  base: 'to-do-fsd',
   resolve: {
     alias: {
       '@app': path.resolve(__dirname, 'src/app'),
