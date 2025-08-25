f5ef817575adb3744bc4463cfa18822753340871
niaz <niazsik@gmail.com>
2025-08-25 16:42:38 +0300

    bug: fix url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'to-do-fsd',
+  base: '/to-do-fsd/',
   resolve: {
     alias: {
       '@app': path.resolve(__dirname, 'src/app'),
