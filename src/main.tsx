71e54d3f9ca2bba17140875b1ee8a98c436b3a2d
niaz <niazsik@gmail.com>
2025-08-27 18:31:00 +0300

    feat: add version

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import { App } from '@app/App'
 import { AppProviders } from '@app/providers'
 import ReactDOM from 'react-dom/client'
+
+import { version as APP_VERSION } from '../package.json'
 import '@shared/styles/global.css'
+
+console.log('--- App started')
+console.log('version:', APP_VERSION)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <AppProviders>
